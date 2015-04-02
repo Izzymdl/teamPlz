dbe7f8ea86e52ff6f6b4df25114457164c596be9
Izzymdl <Izzymdla@gmail.com>
2015-04-02 12:50:28 -0500

    +Eclipse files

---
+++ Eclipse/groupProj/src/groupProj/guiProj.java
@@ -0,0 +1,5 @@
+package groupProj;
+
+public class guiProj {
+
+}
---
+++ Eclipse/groupProj/src/guiProj.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+
+
+public class guiProj {
+
+	
+	
+	public guiProj(){
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() { new guiProj(); }
+		});
+	}
+
+}
